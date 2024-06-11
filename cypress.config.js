317f0696c1f38034ce35be26a134c40b5438cf9c
Felipe <felipeaparecido.f@gmail.com>
2024-06-11 20:10:59 -0300

    Adicionando demais arquivos do cypress

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  viewportWidth: 1000,
+  viewportHeight: 660,
+  e2e: {
+    setupNodeEvents(on, config) {},
+  },
+});
